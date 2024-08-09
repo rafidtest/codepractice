7c5d77cb867157cca29fd741ef759376c3ee138c
rafidtest <rafidfikri@gmail.com>
2024-08-09 23:31:20 +0700

    practice8

--- practice8.cpp
+++ practice8.cpp
@@ -9,10 +9,81 @@ using namespace std;
 
 
 int main() {
+    const int index_row = 0;
+    const int index_column = 1;
+    
+    int num_of_rows = 0;
+    int num_of_queries = 0;
+    int input_number;
+    int output_row, output_column;
+    cin >> num_of_rows >> num_of_queries;
+    cin.ignore();
+    vector<vector<int>> matrix(num_of_rows * num_of_queries);
+    string input_line;
+    
+    for (int i = 0; i < (num_of_rows * num_of_queries); i++) {
+        getline(cin, input_line);
+        istringstream stream(input_line);
+        
+        while (stream >> input_number) {
+            matrix[i].push_back(input_number);
+        }
+        
+        input_line = "";
+    }
+    
+    for (int j = 0; j < num_of_queries; j++) {
+        output_row = matrix[num_of_rows + j][index_row];
+        output_column = matrix[num_of_rows + j][index_column];
+        cout << matrix[output_row][output_column+1];
+        if (j < (num_of_queries-1)) {
+            cout << "\r\n";
+        }
+    }
+    
+    cout << endl;
+
+#if 0
+    cout << "\r\nyour input is:\r\n";
+    for (int iterate : matrix[0]) {
+        cout << iterate << " ";
+    }
+    
+    cout << "\r\n";
+    
+    for (int iterate : matrix[1]) {
+        cout << iterate << " ";
+    }
+
+    cout << "\r\n";
+    
+    for (int iterate : matrix[2]) {
+        cout << iterate << " ";
+    }
+
+    cout << "\r\n";
+    
+    for (int iterate : matrix[3]) {
+        cout << iterate << " ";
+    }
+    
+    cout << endl;
+#endif
+    
+//    for (int i=0; i < num_of_rows; i++) {
+//        
+//    }
+//    
+//    cout << "\r\nyour input is : \r\n";
+//    cout << num_of_rows << " " << num_of_queries << "\r\n";
+    
+    
+    
+#if 0
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int numRows, numQueries; // n and q
     cin >> numRows >> numQueries;
-    std::cin.ignore(); // To ignore the remaining newline character after reading the number of rows
+    cin.ignore(); // To ignore the remaining newline character after reading the number of rows
 
     vector<vector<int>> matrix;
     string line;
@@ -30,7 +101,7 @@ int main() {
         matrix.push_back(row);
     }
     
-    std::cin.ignore(); // To ignore the remaining newline character after reading the number of rows
+    cin.ignore(); // To ignore the remaining newline character after reading the number of rows
    
     vector<pair<int, int>> pairs(numQueries);
     for (int i = 0; i < numQueries; i++) {
@@ -40,7 +111,7 @@ int main() {
     for (int i = 0; i < numQueries; i++) {
         cout << matrix[pairs[i].first][pairs[i].second] << "\r\n";
     }
-    
+#endif
     
     
     
