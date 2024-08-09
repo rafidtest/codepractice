00be68b68d0cd5ae6d187d77c7076e236c0f98d8
rafidtest <rafidfikri@gmail.com>
2024-08-09 23:31:30 +0700

    practice8 correct

--- practice8_correct.cpp
+++ practice8_correct.cpp
@@ -6,29 +6,77 @@
 #include <sstream>
 #include <string>
 using namespace std;
-//solutions using vector
+
 
 int main() {
+    // get length of 2D array and number of queries
+    int n, q;
+    cin >> n >> q;
+
+    // create 2D array
+    int** a = new int*[n]();
+
+    // fill 2D array with 1D subarrays
+    for (int i = 0; i < n; i++) {
+        // get the length of the 1D array at a[i]
+        int k;
+        cin >> k;
+
+        // create the 1D subarray with the given length
+        int* i_arr = new int[k]();
+
+        // fill the subarray with k values
+        for (int j = 0; j < k; j++) {
+            cin >> i_arr[j];
+        }
+
+        // store the subarray in a
+        a[i] = i_arr;
+    }
+
+
+    // run queries on a
+    for (int q_num = 0; q_num < q; q_num++) {
+        // get i, j as the 'query' to get a value from a
+        int i, j;
+        cin >> i >> j;
+        cout << a[i][j] << endl;
+    }
+
+    // delete 2D array (each subarray must be deleted)
+    for (int i = 0; i < n; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
+    
+    
+#if 0
     const int index_row = 0;
     const int index_column = 1;
     
     int num_of_rows = 0;
     int num_of_queries = 0;
-    int input_number;
+    int input_number; int count = 0;
     int output_row, output_column;
     cin >> num_of_rows >> num_of_queries;
     cin.ignore();
-    vector<vector<int>> matrix(num_of_rows * num_of_queries);
+    
+    const int MAX_SIZE = 10;
+    int matrix[MAX_SIZE][MAX_SIZE] = {0};
+    
     string input_line;
     
     for (int i = 0; i < (num_of_rows * num_of_queries); i++) {
         getline(cin, input_line);
         istringstream stream(input_line);
         
-        while (stream >> input_number) {
-            matrix[i].push_back(input_number);
+        while (stream >> input_number && count < MAX_SIZE) {
+            matrix[i][count++] = input_number;
         }
         
+        count = 0;
         input_line = "";
     }
     
@@ -42,7 +90,8 @@ int main() {
     }
     
     cout << endl;
-
+#endif
+    
 #if 0
     cout << "\r\nyour input is:\r\n";
     for (int iterate : matrix[0]) {
@@ -112,7 +161,8 @@ int main() {
         cout << matrix[pairs[i].first][pairs[i].second] << "\r\n";
     }
 #endif
-
+    
+    
     
     return 0;
     
